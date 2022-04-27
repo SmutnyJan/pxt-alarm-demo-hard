a73743928cf4b1eb91321ea45896f379b72478ac
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-04-28 00:13:42 +0200

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,56 @@
-
+alarm.onAlarm(function () {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . # # # .
+        # # # # #
+        . . # . .
+        `)
+    alarm.turnOnAlarmAndBroadcast(onText)
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == onText) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            . # # # .
+            # # # # #
+            . . # . .
+            `)
+        alarm.turnOnAlarmAndBroadcast(onText)
+    } else if (receivedString == offText) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            . # # # .
+            # # # # #
+            . . . . .
+            `)
+        alarm.turnOffAlarmAndBroadcast(offText)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . # # # .
+        # # # # #
+        . . . . .
+        `)
+    alarm.turnOffAlarmAndBroadcast(offText)
+})
+let offText = ""
+let onText = ""
+radio.setGroup(1)
+onText = "alarm_on"
+offText = "alarm_off"
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    . # # # .
+    # # # # #
+    . . . . .
+    `)
